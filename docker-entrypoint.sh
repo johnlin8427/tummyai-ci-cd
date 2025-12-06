a8e0c0d263700a6f22f1c0537a359fb770a36b76
johnlin8427 <john_lin@hms.harvard.edu>
2025-12-06 11:55:09 -0500

    Add DVC

---
+++ docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo "Container is running!!!"
+
+# Mount GCS bucket
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+mkdir -p /mnt/gcs_bucket
+gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_bucket
+echo 'GCS bucket mounted at /mnt/gcs_bucket'
+mkdir -p /app/gcs_data
+mount --bind /mnt/gcs_bucket/data /app/gcs_data
+
+# Activate virtual environment
+echo "Activating virtual environment..."
+source /.venv/bin/activate
+
+# Mark /app as a safe Git directory
+git config --global --add safe.directory /app
+
+# If arguments are passed, execute them instead of starting the server
+if [ $# -gt 0 ]; then
+  echo "Executing command: $@"
+  exec "$@"
+fi
+
+# Otherwise, start an interactive bash shell
+echo "Starting interactive /bin/bash shell"
+exec /bin/bash
---
+++ docker-shell.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# exit immediately if a command exits with a non-zero status
+set -e
+
+# Define some environment variables
+export IMAGE_NAME="tummyai-app"
+export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../secrets/
+export GCP_PROJECT="tummyai-ci-cd"
+export GCS_BUCKET_NAME="tummyai-app-models"
+
+# Create the network if we don't have it yet
+docker network inspect tummyai-app-network >/dev/null 2>&1 || docker network create tummyai-app-network
+
+# Build the image based on the Dockerfile
+docker build -t $IMAGE_NAME -f Dockerfile .
+
+# Run the container
+docker run --rm --name $IMAGE_NAME -ti \
+--cap-add SYS_ADMIN \
+--device /dev/fuse \
+-v "$BASE_DIR":/app \
+-v "$SECRETS_DIR":/secrets \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcp-service.json \
+-e GCP_PROJECT=$GCP_PROJECT \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+--network tummyai-app-network \
+$IMAGE_NAME
