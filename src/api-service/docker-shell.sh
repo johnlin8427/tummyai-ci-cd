63abb4a54b6dfc760589ce9ab425a77cd6196123
johnlin8427 <john_lin@hms.harvard.edu>
2025-12-07 09:44:12 -0500

    Fix GitHub Actions error

--- src/api-service/docker-shell.sh
+++ src/api-service/docker-shell.sh
@@ -15,17 +15,31 @@ export GCS_BUCKET_NAME="tummyai-app-models"
 docker network inspect tummyai-app-network >/dev/null 2>&1 || docker network create tummyai-app-network
 
 # Build the image based on the Dockerfile
-docker build -t $IMAGE_NAME -f Dockerfile .
+# docker build -t $IMAGE_NAME -f Dockerfile .
+
+# # Run the container
+# docker run -d --name api-server -p 9000:9000 \
+# -e DEV=1 \
+# -e GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcp-service.json \
+# -e GCP_PROJECT=$GCP_PROJECT \
+# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+# -v "$SECRETS_DIR":/secrets \
+# $IMAGE_NAME
 
 # Run the container
-docker run --rm --name $IMAGE_NAME -ti \
--v "$BASE_DIR":/app \
--v "$SECRETS_DIR":/secrets \
--v "$PERSISTENT_DIR":/persistent \
--p 9000:9000 \
--e DEV=1 \
--e GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcp-service.json \
--e GCP_PROJECT=$GCP_PROJECT \
--e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
---network tummyai-app-network \
-$IMAGE_NAME
+docker run --rm \
+--network host \
+$IMAGE_NAME pytest tests/system/ -v --tb=short
+
+# # Run the container
+# docker run --rm --name $IMAGE_NAME -ti \
+# -v "$BASE_DIR":/app \
+# -v "$SECRETS_DIR":/secrets \
+# -v "$PERSISTENT_DIR":/persistent \
+# -p 9000:9000 \
+# -e DEV=1 \
+# -e GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcp-service.json \
+# -e GCP_PROJECT=$GCP_PROJECT \
+# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+# --network tummyai-app-network \
+# $IMAGE_NAME
